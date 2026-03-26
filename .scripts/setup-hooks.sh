e381c09e42781351c9cf6007ec17b83f30628fd6
indaco <indaco@users.noreply.github.com>
2026-03-26 18:19:49 +0100

    chore: remove obsolete .scripts hook setup files

--- .scripts/setup-hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set the hooks directory to the `.githooks` folder in the repository
-git config core.hooksPath .githooks
-
-# Make the hooks executable
-chmod +x .githooks/*
-
-echo "Git hooks have been set up."
